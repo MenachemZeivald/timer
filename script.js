f740e9954b0bac618ac88ab68f8fe4e7d5297b62
MenachemZeivald <108309381+MenachemZeivald@users.noreply.github.com>
2022-08-26 01:00:34 +0300

    everything should work :)

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 var frame = document.getElementById("frame");
 var res = document.getElementById("result");
 var line = document.getElementById("line");
-var sec, min, hours, lol, sum, sum1, sum2, sum3;
+var sec, min, hours, lol;
 
 function timer() {
     sec = document.getElementById("seco");
     min = document.getElementById("min");
     hours = document.getElementById("hours");
-    sum1 = Number(sec.value) ? Number(sec.value) : 0; // sum of seconds in sec.val
-    sum2 = Number(min.value) ? (min.value * 60) : 0; // sum of seconds in min.val
-    sum3 = Number(hours.value) ? (sec.value * 3600) : 0; // sum of seconds in hours.val
-    sum = sum1 + sum2 + sum3; // // sum of all seconds
+
+    let sum1 = Number(sec.value) ? Number(sec.value) : 0; // sum of seconds in sec.val
+    let sum2 = Number(min.value) ? (min.value * 60) : 0; // sum of seconds in min.val
+    let sum3 = Number(hours.value) ? (hours.value * 3600) : 0; // sum of seconds in hours.val
+    let sum = sum1 + sum2 + sum3; // // sum of all seconds
+
     clearInterval(lol); // just to fix if the user submit twice
+    linesize(sum); // change the length of the progress line
     lol = setInterval(countDown, 1000); // the main func, call every minut
 }
 
 function countDown() {
     if (Number(sec.value) || Number(min.value) || Number(hours.value)) {
         // change the text value in the input field to the right value or to '00' if there is no value
-        sum1 = Number(sec.value) ? sec.value : 0;
-        sum2 = Number(min.value) ? (min.value * 60) : 0;
-        sum3 = Number(hours.value) ? (sec.value * 3600) : 0;
-        hours.value = (hours.value === "hh" || hours.value == 0) ? "00" : hours.value < 10 ? "0" + hours.value : hours.value;
-        min.value = (min.value === "mm" || min.value == 0) ? "00" : min.value < 10 ? "0" + min.value : min.value;
-        sec.value = (sec.value === "sec" || sec.value == 0) ? "00" : sec.value < 10 ? "0" + sec.value : sec.value;
-        // change the length of the progrress line 
-        linesize();
+        hours.value = (hours.value === "hh" || hours.value == 0) ? "00" : hours.value; // < 10 || hours.value != "0" + hours.value) ? "0" + hours.value : hours.value;
+        min.value = (min.value === "mm" || min.value == 0) ? "00" : min.value; // < 10 || min.value != "0" + min.value) ? "0" + min.value : min.value;
+        sec.value = (sec.value === "sec" || sec.value == 0) ? "00" : sec.value; // < 10 || sec.value != "0" + sec.value) ? "0" + sec.value : sec.value;
         sec.value--;
-        console.log(hours.value + min.value + sec.value);
-        console.log(sum);
         if (sec.value <= 1) {
             if (min.value >= 1) {
                 min.value--;
@@ -40,7 +36,6 @@ function countDown() {
             }
         }
     } else { // if sec + min + hours == 0
-        line.style.width = "0%";
         setTimeout(function () { // return to the initial style
             hours.value = "hh";
             hours.style.color = "rgba(0, 0, 0, 0.55)";
@@ -48,18 +43,16 @@ function countDown() {
             min.style.color = "rgba(0, 0, 0, 0.55)";
             sec.value = "sec";
             sec.style.color = "rgba(0, 0, 0, 0.55)";
-            line.classList.remove("smooth");
-            line.style.width = "80%"; // change the line to the initial size 
-            line.style.transform = "scale(1)";
+            line.style = "";
         }, 1000);
         clearInterval(lol);
     }
 }
 
-function linesize() {
-    line.classList.add("smooth");
+function linesize(numOfSec) {
     const mmObj = window.matchMedia("(max-width: 660px)"); // media quary, to change the line horizotally in web and verticaly in mobile 
-    mmObj.matches ? line.style.transform = `scaleY(${(((sum1 + sum2 + sum3) / (sum)) / 100)})` : line.style.transform = `scaleX(${(((sum1 + sum2 + sum3) / (sum)) / 100)})`;
+    mmObj.matches ? line.style.top = "100%" : line.style.left = "-100%";
+    line.style.transition = `all linear ${numOfSec}s`;
 }
 
 document.getElementById("min").onblur = function blur() {
